8aa9bf43fba23a0984c0f97c58c25498ba1e14ed
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-09-13 19:16:09 +0900

    Added Adaptor

---
+++ design-pattern/src/main/java/com/orcl/Application.java
@@ -0,0 +1,41 @@
+package com.orcl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.orcl.design.adaptor.Print;
+import com.orcl.design.adaptor.PrintHuman;
+import com.orcl.design.singleton.TicketMaker;
+
+/**
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {  
+        /**
+         * Adaptor Pattern CLient
+         */
+        //Print p = new PrintHuman("Hoge", 50);
+        //p.printName();
+        //p.printAge();
+
+        /* Singleton */
+        TicketMaker instance1 = TicketMaker.getInstance();
+        TicketMaker instance2 = TicketMaker.getInstance();
+        System.out.println(instance1);
+        System.out.println(instance2);
+
+        List<TicketMaker> lst = new ArrayList<TicketMaker>();
+        lst.add(TicketMaker.getInstance());
+        lst.add(TicketMaker.getInstance());
+        lst.add(TicketMaker.getInstance());
+
+        Iterator<TicketMaker> it = lst.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}

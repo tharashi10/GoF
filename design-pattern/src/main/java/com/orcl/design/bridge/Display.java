773c42dddfff2520da762c3a85994d32d7ebae22
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-11-09 18:10:12 +0900

    09

--- design-pattern/src/main/java/com/orcl/design/bridge/Display.java
+++ design-pattern/src/main/java/com/orcl/design/bridge/Display.java
@@ -1,5 +1,7 @@
 package com.orcl.design.bridge;
 
+// 機能: 環境など依存性を気にせず、共通で使えるもの（たぶん）
+// 実装: 環境など、仕様上気にする必要が出てくるもの。MacImpl, WinImpl（たぶん）
 // 機能のクラス階層 = 抽象的なイメージで「何かを表示するもの」
 public class Display {
     private DisplayImpl impl;

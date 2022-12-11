7d5b55e893d11cb8d322fb9c66b0135c25ff33f8
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-11 21:46:29 +0900

    cpp

--- cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tharashi/2022/GoF
-# Build directory: /Users/tharashi/2022/GoF/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
